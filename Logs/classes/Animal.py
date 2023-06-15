fb4cf974b5f03ebb2e72db0d5a723fd9dd38b85a
nina <nina777ter@gmail.com>
2023-06-15 19:10:36 -0400

    selenium

---
+++ Logs/classes/Animal.py
@@ -0,0 +1,17 @@
+
+
+
+class Animal:
+    def __int__(self, color, food_type):
+        print("I am init.")
+        self.color = color
+        self.food_type = food_type
+
+
+
+
+my_animal1 = Animal('blue', 'meat')
+my_animal2 = Animal('red', 'veg')
+
+print(my_animal1)
+print(my_animal2)
