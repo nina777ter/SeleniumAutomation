10d4cbc81858c817f0993042589ac231415ec30a
nina <nina777ter@gmail.com>
2023-06-15 21:04:07 -0400

    selenium

---
+++ google_search.py
@@ -0,0 +1,6 @@
+Google search manual test (needs to be automated)
+Launch the Chrome browser
+open the https://www.google.com page
+Search for 'ai' and hit enter
+save the search result > About 10,980,000,000 results (0.66 seconds)
+close the browser
